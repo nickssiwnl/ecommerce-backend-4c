37b9ecf8bc9774ccb6e9dfa0dd820757c953d0ae
Sérgio Roberto Delfino <sergio.delfino@fatecourinhos.edu.br>
2026-08-26 01:36:08 +0000

    Codificação das Entidades

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into categoria (nome, descricao) values ('Informática', 'Produtos de Informática');
+insert into categoria (nome, descricao) values ('Livros', 'Livros Técnicos');
+insert into categoria (nome, descricao) values ('Eletrônicos', 'Aparelhos Eletrônicos');
+
+insert into produto (nome, descricao, preco, estoque, categoria_id) values ('Código Limpo', 'Livro do Autor Robert C. Martin', 100.00, 20, 2);
