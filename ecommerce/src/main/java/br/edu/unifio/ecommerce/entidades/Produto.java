393e13d45c25490f1e55cfab00ddf716b38e1d22
Nicolas Miguel <nickssiwnl007@gmail.com>
2026-09-07 18:42:19 +0000

    CMC

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Produto.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Produto.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
